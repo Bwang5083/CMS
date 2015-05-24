25c88597cc374411a18f8ce77aa7ca8bc42f1d23
Jason <Bwang5083@conestogac.on.ca>
2015-05-24 16:29:52 -0400

    1

--- common/action.class.php
+++ common/action.class.php
@@ -1,12 +1,86 @@
 <?php
 class action extends mysql {
-public function get_show_msg($url, $show = '操作已成功！') {
+
+	/**
+	 * 用户权限判断($uid, $shell, $m_id)
+	 */
+
+	public function Get_user_shell($uid, $shell) {
+		$query = $this->select('p_admin', '*', '`uid` = \'' . $uid . '\'');
+		$us = is_array($row = $this->fetch_array($query));
+		$shell = $us ? $shell == md5($row[username] . $row[password] . "TKBK") : FALSE;
+		return $shell ? $row : NULL;
+	} //end shell
+
+	public function Get_user_shell_check($uid, $shell, $m_id = 9) {
+		if ($row=$this->Get_user_shell($uid, $shell)) {
+			if ($row[m_id] <= $m_id) {
+				return $row;
+			} else {
+				echo "你无权限操作！";
+				exit ();
+			} //end m_id
+		} else {
+		  $this->Get_admin_msg('index.php','请先登陆');
+		}
+	} //end shell
+	//========================================
+
+
+	/**
+	 * 用户登陆超时时间(秒)
+	 */
+	public function Get_user_ontime($long = '3600') {
+		$new_time = mktime();
+		$onlinetime = $_SESSION[ontime];
+		echo $new_time - $onlinetime;
+		if ($new_time - $onlinetime > $long) {
+			echo "登录超时";
+			session_destroy();
+			exit ();
+		} else {
+			$_SESSION[ontime] = mktime();
+		}
+	}
+
+	/**
+	 * 用户登陆
+	 */
+	public function Get_user_login($username, $password) {
+		$username = str_replace(" ", "", $username);
+		$query = $this->select('p_admin', '*', '`username` = \'' . $username . '\'');
+		$us = is_array($row = $this->fetch_array($query));
+		;
+		$ps = $us ? ($password) == $row[password] : FALSE;
+		if ($ps) {
+			$_SESSION[uid] = $row[uid];
+			$_SESSION[shell] = md5($row[username] . $row[password] . "TKBK");
+			$_SESSION[ontime] = mktime();
+			$_SESSION[username] = $row[username];
+			$this->Get_admin_msg('main.php','登陆成功！');
+		} else {
+			$this->Get_admin_msg('index.php','密码或用户错误！');
+			session_destroy();
+		}
+	}
+	 /**
+	  * 用户退出登陆
+	  */
+	public function Get_user_out() {
+		session_destroy();
+		$this->Get_admin_msg('index.php','退出成功！');
+	}
+
+   /**
+    * 后台通用信息提示
+    */
+	public function Get_admin_msg($url, $show = '操作已成功！') {
 		$msg = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml"><head>
 				<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-				<link rel="stylesheet" href="Public/common.css" type="text/css" />
-				<meta http-equiv="refresh" content="2; url=' . $url . '" />
-				<title>消息提示</title>
+				<link rel="stylesheet" href="css/common.css" type="text/css" />
+				<meta http-equiv="refresh" content="2; URL=' . $url . '" />
+				<title>管理区域</title>
 				</head>
 				<body>
 				<div id="man_zone">
@@ -27,8 +101,8 @@ public function get_show_msg($url, $show = '操作已成功！') {
 		exit ();
 	}
 
+	//========================
 } //end class
-
 ?>
 
 
